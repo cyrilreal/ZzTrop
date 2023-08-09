12d6a0c68d183d36db3c594382cfd5e1cabd7b58
cyril <cyril.real@gmail.com>
2023-08-09 19:02:48 +0200

    v1.6 final

--- app/src/main/java/aero/zztrop/ScreenResultCockpit.java
+++ app/src/main/java/aero/zztrop/ScreenResultCockpit.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+
 import androidx.fragment.app.FragmentActivity;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,111 +18,108 @@ import android.widget.ListView;
 import android.widget.ToggleButton;
 
 public class ScreenResultCockpit extends FragmentActivity implements
-		OnClickListener {
+        OnClickListener {
 
-	private ListView lvResults;
-	private TimePairCockpitAdapter m_adapter;
-	private Button btnResultsOK;
-	private ToggleButton tbtnRound;
-	private Button btnShiftPlus, btnShiftMinus;
-	private Button btnDurationPlus, btnDurationMinus;
-	private ZzEngineCockpit zzEngCock;
+    private ListView lvResults;
+    private TimePairCockpitAdapter m_adapter;
+    private Button btnResultsOK;
+    private ToggleButton tbtnRound;
+    private Button btnShiftPlus, btnShiftMinus;
+    private Button btnDurationPlus, btnDurationMinus;
+    private ZzEngineCockpit zzEngCock;
 
-	Context context = ScreenResultCockpit.this.getBaseContext();
+    Context context = ScreenResultCockpit.this.getBaseContext();
 
-	protected void onCreate(Bundle savedInstanceState) {
-		Utils.applySharedTheme(this);
-		super.onCreate(savedInstanceState);
-		if (!Utils.isTablet(getApplicationContext())) {
-			// set phones to portrait;
-			setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-		}
-		setContentView(R.layout.screen_result_cockpit);
-		initComponents();
-		// load user preferences
-		SharedPreferences prefs = getSharedPreferences(Utils.SHARED_PREFS_NAME,
-				MODE_PRIVATE);
-		tbtnRound.setChecked(prefs.getBoolean("roundTimes", false));
+    protected void onCreate(Bundle savedInstanceState) {
+        Utils.applySharedTheme(this);
+        super.onCreate(savedInstanceState);
+        if (!Utils.isTablet(getApplicationContext())) {
+            // set phones to portrait;
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+        setContentView(R.layout.screen_result_cockpit);
+        initComponents();
+        // load user preferences
+        SharedPreferences prefs = getSharedPreferences(Utils.SHARED_PREFS_NAME, MODE_PRIVATE);
+        tbtnRound.setChecked(prefs.getBoolean("roundTimes", false));
 
-		zzEngCock.computeRest();
-		if (tbtnRound.isChecked()) {
-			zzEngCock.roundTimes();
-		}
-		// prepare the list to be displayed
-		populateList();
-		//createAlarm();
-	}
+        zzEngCock.computeRest();
+        if (tbtnRound.isChecked()) {
+            zzEngCock.roundTimes();
+        }
+        // prepare the list to be displayed
+        populateList();
+        //createAlarm();
+    }
 
-	@Override
-	protected void onStop() {
-		super.onStop();
+    @Override
+    protected void onStop() {
+        super.onStop();
 
-		// Save user preferences
-		SharedPreferences prefs = getSharedPreferences(Utils.SHARED_PREFS_NAME,
-				MODE_PRIVATE);
-		SharedPreferences.Editor editor = prefs.edit();
-		editor.putInt("startupMode", 0); // 0 = cockpit mode
-		editor.putBoolean("roundTimes", tbtnRound.isChecked());
-		editor.commit();
-	}
+        // Save user preferences
+        SharedPreferences prefs = getSharedPreferences(Utils.SHARED_PREFS_NAME,
+                MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("startupMode", 0); // 0 = cockpit mode
+        editor.putBoolean("roundTimes", tbtnRound.isChecked());
+        editor.commit();
+    }
 
-	@Override
-	protected void onDestroy() {
-		// am.cancel(pi);
-		// unregisterReceiver(br);
-		super.onDestroy();
-	}
+    @Override
+    protected void onDestroy() {
+        // am.cancel(pi);
+        // unregisterReceiver(br);
+        super.onDestroy();
+    }
 
-	private void initComponents() {
+    private void initComponents() {
 
-		zzEngCock = new ZzEngineCockpit();
-		// load engine with passed parameters
-		Bundle bundle = this.getIntent().getExtras();
-		zzEngCock.setPattern(bundle.getString("pattern"));
-		zzEngCock.setCalStart(bundle.getString("time_start"));
-		zzEngCock.setCalEnd(bundle.getString("time_end"));
-		zzEngCock.setPause(bundle.getInt("pause"));
-		zzEngCock.setxTime(bundle.getLong("x_time"));
-		zzEngCock.setAfterTkf(bundle.getInt("after_tkf", 0));
-		zzEngCock.setBeforeLdg(bundle.getInt("before_ldg", 0));
-		zzEngCock.adjustStartEnd();
-		btnResultsOK = (Button) findViewById(R.id.btnResultsOK);
-		btnResultsOK.setOnClickListener(this);
-		btnShiftMinus = (Button) findViewById(R.id.btnShiftMinus);
-		btnShiftMinus.setOnClickListener(this);
-		btnShiftPlus = (Button) findViewById(R.id.btnShiftPlus);
-		btnShiftPlus.setOnClickListener(this);
-		btnDurationMinus = (Button) findViewById(R.id.btnDurationMinus);
-		btnDurationMinus.setOnClickListener(this);
-		btnDurationPlus = (Button) findViewById(R.id.btnDurationPlus);
-		btnDurationPlus.setOnClickListener(this);
-		tbtnRound = (ToggleButton) findViewById(R.id.tbtnRound);
-		tbtnRound.setOnClickListener(this);
+        zzEngCock = new ZzEngineCockpit();
+        // load engine with passed parameters
+        Bundle bundle = this.getIntent().getExtras();
+        zzEngCock.setPattern(bundle.getString("pattern"));
+        zzEngCock.setCalStart(bundle.getString("time_start"));
+        zzEngCock.setCalEnd(bundle.getString("time_end"));
+        zzEngCock.setPause(bundle.getInt("pause"));
+        zzEngCock.setxTime(bundle.getLong("x_time"));
+        zzEngCock.setAfterTkf(bundle.getInt("after_tkf", 0));
+        zzEngCock.setBeforeLdg(bundle.getInt("before_ldg", 0));
+        zzEngCock.adjustStartEnd();
+        btnResultsOK = (Button) findViewById(R.id.btnResultsOK);
+        btnResultsOK.setOnClickListener(this);
+        btnShiftMinus = (Button) findViewById(R.id.btnShiftMinus);
+        btnShiftMinus.setOnClickListener(this);
+        btnShiftPlus = (Button) findViewById(R.id.btnShiftPlus);
+        btnShiftPlus.setOnClickListener(this);
+        btnDurationMinus = (Button) findViewById(R.id.btnDurationMinus);
+        btnDurationMinus.setOnClickListener(this);
+        btnDurationPlus = (Button) findViewById(R.id.btnDurationPlus);
+        btnDurationPlus.setOnClickListener(this);
+        tbtnRound = (ToggleButton) findViewById(R.id.tbtnRound);
+        tbtnRound.setOnClickListener(this);
 
-		lvResults = (ListView) findViewById(R.id.lvResults);
-		// add a header to the list
-		LayoutInflater inflater = getLayoutInflater();
-		ViewGroup header = (ViewGroup) inflater.inflate(
-				R.layout.result_header, lvResults, false);
-		lvResults.addHeaderView(header, null, false);
-	}
+        lvResults = (ListView) findViewById(R.id.lvResults);
+        // add a header to the list
+        LayoutInflater inflater = getLayoutInflater();
+        ViewGroup header = (ViewGroup) inflater.inflate(
+                R.layout.result_header, lvResults, false);
+        lvResults.addHeaderView(header, null, false);
+    }
 
-	private void populateList() {
-		// the view is defined in the TimePairAdapter class
-		m_adapter = new TimePairCockpitAdapter(this, 0,
-				zzEngCock.getAlTimePairs());
+    private void populateList() {
+        // the view is defined in the TimePairAdapter class
+        m_adapter = new TimePairCockpitAdapter(this, 0, zzEngCock.getAlTimePairs());
 
-		lvResults.setAdapter(m_adapter);
-		lvResults.setOnItemClickListener(new OnItemClickListener() {
+        lvResults.setAdapter(m_adapter);
+        lvResults.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-					long arg3) {
-				zzEngCock.selectTimePair(arg2 - 1);
-				m_adapter.notifyDataSetChanged();
-			}
-		});
-	}
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                zzEngCock.selectTimePair(arg2 - 1);
+                m_adapter.notifyDataSetChanged();
+            }
+        });
+    }
 
 //	private void createAlarm() {
 //		TimePair tp = zzEngCock.getAlTimePairs().get(0);
@@ -168,96 +167,96 @@ public class ScreenResultCockpit extends FragmentActivity implements
 //				.show();
 //	}
 
-	@Override
-	public void onClick(View v) {
-		if (v == btnResultsOK) {
-			finish();
-		}
+    @Override
+    public void onClick(View v) {
+        if (v == btnResultsOK) {
+            finish();
+        }
 
-		if (v == tbtnRound) {
-			if (tbtnRound.isChecked()) {
-				zzEngCock.roundTimes();
-				m_adapter.notifyDataSetChanged();
-			} else {
-				zzEngCock.computeRest();
-				m_adapter.notifyDataSetChanged();
-			}
+        if (v == tbtnRound) {
+            if (tbtnRound.isChecked()) {
+                zzEngCock.roundTimes();
+                m_adapter.notifyDataSetChanged();
+            } else {
+                zzEngCock.computeRest();
+                m_adapter.notifyDataSetChanged();
+            }
 //			createAlarm();
-		}
+        }
 
-		if (v == btnShiftMinus) {
-			if (tbtnRound.isChecked()) {
-				zzEngCock.shiftTimes(-5);
-			} else
-				zzEngCock.shiftTimes(-1);
+        if (v == btnShiftMinus) {
+            if (tbtnRound.isChecked()) {
+                zzEngCock.shiftTimes(-5);
+            } else
+                zzEngCock.shiftTimes(-1);
 
-			m_adapter.notifyDataSetChanged();
-//			createAlarm();
-
-		}
-
-		if (v == btnShiftPlus) {
-			if (tbtnRound.isChecked()) {
-				zzEngCock.shiftTimes(5);
-			} else
-				zzEngCock.shiftTimes(1);
-
-			m_adapter.notifyDataSetChanged();
+            m_adapter.notifyDataSetChanged();
 //			createAlarm();
 
-		}
+        }
 
-		if (v == btnDurationMinus) {
-			if (tbtnRound.isChecked()) {
-				if (isTimePairSelection()) {
-					zzEngCock.modifyTimePair(zzEngCock.getSelectedTimePair(),
-							-5);
-				} else {
-					zzEngCock.modifyAllTimes(-5);
-				}
-			} else {
-				if (isTimePairSelection()) {
-					zzEngCock.modifyTimePair(zzEngCock.getSelectedTimePair(),
-							-1);
-				} else {
-					zzEngCock.modifyAllTimes(-1);
-				}
-			}
+        if (v == btnShiftPlus) {
+            if (tbtnRound.isChecked()) {
+                zzEngCock.shiftTimes(5);
+            } else
+                zzEngCock.shiftTimes(1);
 
-			m_adapter.notifyDataSetChanged();
+            m_adapter.notifyDataSetChanged();
 //			createAlarm();
 
-		}
+        }
 
-		if (v == btnDurationPlus) {
-			if (tbtnRound.isChecked()) {
-				if (isTimePairSelection()) {
-					zzEngCock
-							.modifyTimePair(zzEngCock.getSelectedTimePair(), 5);
-				} else {
-					zzEngCock.modifyAllTimes(5);
-				}
-			} else {
-				if (isTimePairSelection()) {
-					zzEngCock
-							.modifyTimePair(zzEngCock.getSelectedTimePair(), 1);
-				} else {
-					zzEngCock.modifyAllTimes(1);
-				}
-			}
+        if (v == btnDurationMinus) {
+            if (tbtnRound.isChecked()) {
+                if (isTimePairSelection()) {
+                    zzEngCock.modifyTimePair(zzEngCock.getSelectedTimePair(),
+                            -5);
+                } else {
+                    zzEngCock.modifyAllTimes(-5);
+                }
+            } else {
+                if (isTimePairSelection()) {
+                    zzEngCock.modifyTimePair(zzEngCock.getSelectedTimePair(),
+                            -1);
+                } else {
+                    zzEngCock.modifyAllTimes(-1);
+                }
+            }
 
-			m_adapter.notifyDataSetChanged();
+            m_adapter.notifyDataSetChanged();
 //			createAlarm();
 
-		}
-	}
+        }
 
-	private boolean isTimePairSelection() {
-		for (TimePair tp : zzEngCock.getAlTimePairs()) {
-			if (tp.isSelected()) {
-				return true;
-			}
-		}
-		return false;
-	}
+        if (v == btnDurationPlus) {
+            if (tbtnRound.isChecked()) {
+                if (isTimePairSelection()) {
+                    zzEngCock
+                            .modifyTimePair(zzEngCock.getSelectedTimePair(), 5);
+                } else {
+                    zzEngCock.modifyAllTimes(5);
+                }
+            } else {
+                if (isTimePairSelection()) {
+                    zzEngCock
+                            .modifyTimePair(zzEngCock.getSelectedTimePair(), 1);
+                } else {
+                    zzEngCock.modifyAllTimes(1);
+                }
+            }
+
+            m_adapter.notifyDataSetChanged();
+//			createAlarm();
+
+        }
+    }
+
+    private boolean isTimePairSelection() {
+        for (TimePair tp : zzEngCock.getAlTimePairs()) {
+            if (tp.isSelected()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
